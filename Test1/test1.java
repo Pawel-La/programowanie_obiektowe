51f4d88b47bebabdf551f6964632a0691baa8425
Paweł <pawellamza@gmail.com>
2022-10-04 13:59:19 +0200

    testing commiting and pushing

--- Test1/test1.java
+++ Test1/test1.java
@@ -4,5 +4,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hi there");
+        System.out.println("what is up");
     }
 }
