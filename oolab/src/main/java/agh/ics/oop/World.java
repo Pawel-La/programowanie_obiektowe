f62b134116dbad04500a7cf7ea9d6c2decaa8bfd
Paweł <pawellamza@gmail.com>
2022-10-10 15:04:32 +0200

    added some corrections

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,8 +1,7 @@
 package agh.ics.oop;
-import agh.ics.oop.Direction.direction;
 
 public class World {
-    public static void move(direction arg){
+    public static void move(Direction arg){
         String message = switch (arg) {
             case FORWARD -> "Zwierzak idzie do przodu";
             case BACKWARD -> "Zwierzak idzie do tyłu";
@@ -11,29 +10,25 @@ public class World {
             default -> "None";
         };
         if (!message.equals("None")) {
-            System.out.print(message);
-            System.out.println();
+            System.out.println(message);
         }
     }
-    public static void run(direction[] args) {
-        for(int i = 0; i < args.length - 1; i++) {
-            move(args[i]);
+    public static void run(Direction[] args) {
+        for(Direction arg: args) {
+            move(arg);
         }
-        move(args[args.length - 1]);
     }
-    public static direction[] transform_to_enum(String[] args) {
-        direction[] result = new direction[args.length];
-        direction dir;
+    public static Direction[] transform_to_enum(String[] args) {
+        Direction[] result = new Direction[args.length];
 
         for (int i = 0; i < args.length; i++) {
-            dir = switch (args[i]){
-                case "f" -> direction.FORWARD;
-                case "b" -> direction.BACKWARD;
-                case "r" -> direction.RIGHT;
-                case "l" -> direction.LEFT;
-                default -> direction.NONE;
+            result[i] = switch (args[i]){
+                case "f" -> Direction.FORWARD;
+                case "b" -> Direction.BACKWARD;
+                case "r" -> Direction.RIGHT;
+                case "l" -> Direction.LEFT;
+                default  -> Direction.NONE;
             };
-            result[i] = dir;
         }
 
         return result;
@@ -41,10 +36,10 @@ public class World {
 
     public static void main(String[] args) {
         String[] arguments = {"f", "f", "r", "l"};
-        direction[] transformed = transform_to_enum(arguments);
+        Direction[] transformed = transform_to_enum(arguments);
 
         System.out.println("Start");
         run(transformed);
-        System.out.println("Stop");
+        System.out.print("Stop");
     }
 }
