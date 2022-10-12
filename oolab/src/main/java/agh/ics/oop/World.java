31e2a749c63cdf40ef33f5b6e5df95bf8af2f2b5
Paweł <pawellamza@gmail.com>
2022-10-12 22:59:36 +0200

    changes for labs nr.2

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,5 +1,6 @@
 package agh.ics.oop;
 
+import java.util.Scanner;
 public class World {
     public static void move(Direction arg){
         String message = switch (arg) {
@@ -7,11 +8,8 @@ public class World {
             case BACKWARD -> "Zwierzak idzie do tyłu";
             case RIGHT -> "Zwierzak skręca w prawo";
             case LEFT -> "Zwierzak skręca w lewo";
-            default -> "None";
         };
-        if (!message.equals("None")) {
-            System.out.println(message);
-        }
+        System.out.println(message);
     }
     public static void run(Direction[] args) {
         for(Direction arg: args) {
@@ -19,25 +17,52 @@ public class World {
         }
     }
     public static Direction[] transform_to_enum(String[] args) {
-        Direction[] result = new Direction[args.length];
+        int outLength = 0;
+        for (String arg : args) {
+            if (arg.equals("f") || arg.equals("b") || arg.equals("r") || arg.equals("l")) {
+                outLength++;
+            }
+        }
 
-        for (int i = 0; i < args.length; i++) {
-            result[i] = switch (args[i]){
+        Direction[] result = new Direction[outLength];
+        Direction temporary;
+        int i = 0;
+
+        for (String arg: args) {
+            temporary = switch (arg) {
                 case "f" -> Direction.FORWARD;
                 case "b" -> Direction.BACKWARD;
                 case "r" -> Direction.RIGHT;
                 case "l" -> Direction.LEFT;
-                default  -> Direction.NONE;
+                default -> null;
             };
+            if (temporary != null) {
+                result[i] = temporary;
+                i++;
+            }
         }
-
         return result;
     }
 
     public static void main(String[] args) {
-        String[] arguments = {"f", "f", "r", "l"};
-        Direction[] transformed = transform_to_enum(arguments);
+//        testowanie działania metod z enuma MapDirection
+//        MapDirection direction1 = MapDirection.NORTH;
+//        System.out.println(direction1);
+//        System.out.println(direction1.next());
+//        System.out.println(direction1.previous());
+//        System.out.println(direction1.toUnitVector());
 
+//        Vector2d position1 = new Vector2d(1,2);
+//        System.out.println(position1);
+//        Vector2d position2 = new Vector2d(-2,1);
+//        System.out.println(position2);
+//        System.out.println(position1.add(position2));
+
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        String [] arguments = input.split("\\s+");
+
+        Direction[] transformed = transform_to_enum(arguments);
         System.out.println("Start");
         run(transformed);
         System.out.print("Stop");
---
+++ oolab/src/test/java/agh/ics/oop/MapDirectionTest.java
@@ -0,0 +1,24 @@
+package agh.ics.oop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MapDirectionTest {
+    private final String errorMessage = "Test failed!";
+
+    @Test
+    public void nextTest(){
+        Assertions.assertEquals(MapDirection.NORTH.next(), MapDirection.EAST, errorMessage);
+        Assertions.assertEquals(MapDirection.EAST.next(), MapDirection.SOUTH, errorMessage);
+        Assertions.assertEquals(MapDirection.SOUTH.next(), MapDirection.WEST, errorMessage);
+        Assertions.assertEquals(MapDirection.WEST.next(), MapDirection.NORTH, errorMessage);
+    }
+
+    @Test
+    public void previousTest(){
+        Assertions.assertEquals(MapDirection.NORTH.previous(), MapDirection.WEST, errorMessage);
+        Assertions.assertEquals(MapDirection.EAST.previous(), MapDirection.NORTH, errorMessage);
+        Assertions.assertEquals(MapDirection.SOUTH.previous(), MapDirection.EAST, errorMessage);
+        Assertions.assertEquals(MapDirection.WEST.previous(), MapDirection.SOUTH, errorMessage);
+    }
+}
